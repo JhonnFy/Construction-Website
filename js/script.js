dd8d1184367d41294747e655b7d385d409663301
Jhon <dijhonsdeveloper1023@gmail.com>
2022-01-16 01:17:36 -0500

    ClientsLogoOk

--- js/script.js
+++ js/script.js
@@ -45,36 +45,56 @@ var swiper = new Swiper(".home-slider", {
   },
 });
 
-var swiper = new Swiper(".reviews-slider",{
-    loop:true,
-    grabCursor:true,
-    spaceBetween: 20,
-    breakpoints:{
-        640:{
-            slidesPerView:1,
-        },
-        768:{
-            slidesPerView: 2,
-        },
-        991:{
-            slidesPerView:3,
-        },
+var swiper = new Swiper(".reviews-slider", {
+  loop: true,
+  grabCursor: true,
+  spaceBetween: 20,
+  breakpoints: {
+    640: {
+      slidesPerView: 1,
     },
+    768: {
+      slidesPerView: 2,
+    },
+    991: {
+      slidesPerView: 3,
+    },
+  },
 });
 
-var swiper = new Swiper(".blogs-slider",{
-  loop:true,
-  grabCursor:true,
+var swiper = new Swiper(".blogs-slider", {
+  loop: true,
+  grabCursor: true,
   spaceBetween: 20,
-  breakpoints:{
-      640:{
-          slidesPerView:1,
-      },
-      768:{
-          slidesPerView: 2,
-      },
-      991:{
-          slidesPerView:3,
-      },
+  breakpoints: {
+    640: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    991: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+var swiper = new Swiper(".logo-slider", {
+  loop: true,
+  grabCursor: true,
+  spaceBetween: 20,
+  breakpoints: {
+    450: {
+      slidesPerView: 2,
+    },
+    640: {
+      slidesPerView: 3,
+    },
+    768: {
+      slidesPerView: 4,
+    },
+    1000: {
+      slidesPerView: 5,
+    },
   },
 });
